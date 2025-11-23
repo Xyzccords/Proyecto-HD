37f39b625b02c590bbd911021bb7b40e468c9f26
Xyzccords <Xyzccords-music@hotmail.com>
2025-11-22 19:47:02 -0500

    fix: ajusta los scripts HTML para soportar modulos ES . fix #1

--- student.php
+++ student.php
@@ -175,7 +175,7 @@ include("connection.php");
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
-  <script src = "js/search.js"></script>
+  <script type = "module" src = "js/search.js"></script>
 </body>
 </html>
 
